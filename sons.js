1b0feb6a2c93e5e3e64d2cb6d8bc98b22a2b2aab
PeyreGuillaume34 <peyreguillaume34@gmail.com>
2025-11-17 19:25:42 +0100

    UPDATE : Probleme de son réglé

--- sons.js
+++ sons.js
@@ -1,14 +1,14 @@
-function playVrai() {
+function playVrai(soundEnabled = true) {
     // Vérifier si le son est activé (variable importée depuis game.js)
-    if (window.isSoundEnabled !== false) {
+    if (soundEnabled !== false) {
         let vrai = new Audio('./Sound/BIRDTrop_Piauhau hurleur (ID 1762)_LS.mp3');
         vrai.play();
     }
 }
 
-function playFaux() {
+function playFaux(soundEnabled = true) {
     // Vérifier si le son est activé (variable importée depuis game.js)
-    if (window.isSoundEnabled !== false) {
+    if (soundEnabled !== false) {
         let faux = new Audio('./Sound/BIRDSong_Geai des chenes 1 (ID 3453)_LS.mp3');
         faux.play();
     }
